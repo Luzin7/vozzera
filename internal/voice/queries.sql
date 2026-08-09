b885b51f99109fa1bb0b7b8f4af13490b288f1c5
Luan Victor <lvictor.dutra@gmail.com>
2026-08-09 14:54:38 -0300

    add: create SQL queries for retrieving voice rooms

---
+++ internal/voice/queries.sql
@@ -0,0 +1,5 @@
+-- name: GetRoomByID :one
+SELECT id, name, type, created_at FROM rooms WHERE id = $1 LIMIT 1;
+
+-- name: ListVoiceRooms :many
+SELECT id, name, type, created_at FROM rooms WHERE type = 'voice' ORDER BY name ASC;
