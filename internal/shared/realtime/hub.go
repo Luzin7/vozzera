8a7a46bab84ffbaae08a3556296daf6e8b02c63c
Luan Victor <lvictor.dutra@gmail.com>
2026-09-15 10:33:20 -0300

    fix: remove HandleTopicSubscribed call imported erroneamente no merge da PR #74

--- internal/shared/realtime/hub.go
+++ internal/shared/realtime/hub.go
@@ -208,16 +208,6 @@ func (h *Hub) Run() {
 
 			h.topics[sub.topic][sub.client] = true
 			sub.client.Topics[sub.topic] = true
-			if h.presence != nil {
-				data := h.presence.HandleTopicSubscribed(sub.topic)
-				if data != nil {
-					select {
-					case sub.client.send <- data:
-					default:
-						h.removeClient(sub.client)
-					}
-				}
-			}
 
 		case unsub := <-h.unsubscribe:
 			if clients, ok := h.topics[unsub.topic]; ok {
